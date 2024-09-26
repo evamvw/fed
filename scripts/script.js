3f1a2dcf58a81cf7d515fd71ca60e53bef501f49
evamvw <145342385+evamvw@users.noreply.github.com>
2024-09-26 21:56:55 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,16 @@
 // JavaScript Document
-console.log("hi");
+
+const hamburgerMenu = document.getElementById('hamburgerMenu')
+const sluiten = document.getElementById('sluiten')
+const menu = document.querySelector('header nav article')
+
+hamburgerMenu.addEventListener('click', menuSlider)
+sluiten.addEventListener('click', closeMenu)
+
+function menuSlider(){
+menu.classList.toggle('active')
+}
+
+function closeMenu(){
+    menu.classList.remove('active')
+}
